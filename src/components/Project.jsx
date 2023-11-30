37911814810a5b4e4a5c29981b7c8c3f59670839
Kayla <sullenspartyof3@gmail.com>
2023-11-30 13:25:56 -0800

    Project/Portfolio done (no styling)

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,22 +1,24 @@
-export const Project = ({ name, description, href, imgSrc, imgAlt }) => {
+export const Project = ({ name, description, application, repo, imgSrc, imgAlt }) => {
   return (
     <div className="project-info">
-      <div className="project-img">
-        <a href={href}>
-          <img
-            src={imgSrc}
-            className=""
-            style={{ width: "100%" }}
-            alt={imgAlt}
-          />
-        </a>
-      </div>
+      
       <div className="project-text">
         <h4>{name}</h4>
         <p>
           {description}
         </p>
       </div>
+      <div className="project-img">
+          <img
+            src={imgSrc}
+            className="images"
+            style={{ width: "50%" }}
+            alt={imgAlt}
+          />
+          <a href={application} className="websiteBtn"><button>Application</button></a>
+          <a href={repo} className="websiteBtn"><button>GitHub Repo</button></a>
+      </div>
     </div>
   );
 }
+
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,4 @@
 
-
-// Import images from assets/image folder once added
 import { Project } from '../components/Project'
 
 function Portfolio() {
@@ -10,14 +8,14 @@ function Portfolio() {
         <div className="flex-row">
           <h2 className="section-title">Projects</h2>
         </div>
-
         <div className="projects">
-          <Project name={'CSS Snippet Cheatsheet'} description={'This was a mini project I completed with the help of a walkthrough video, to practice and showcase CSS skills.'} imgAlt={'Image of 6 CSS code snippet guides.'} imgSrc={'css-image.png'} href={'https://github.com/Kayla-Sullens/CSS-Snippet-Cheat-Sheet'} />
-          <Project name={''} description={''} imgAlt={''} imgSrc={''} href={''} />
-          <Project name={''} description={''} imgAlt={''} imgSrc={''} href={''} />
-          <Project name={''} description={''} imgAlt={''} imgSrc={''} href={''} />
-          <Project name={''} description={''} imgAlt={''} imgSrc={''} href={''} />
-          <Project name={''} description={''} imgAlt={''} imgSrc={''} href={''} />
+          <Project name={'Workday Scheduler'} description={'This application allows users to add/delete tasks for the current day.'} imgAlt={'Image of an hourly scheduler.'} imgSrc={'./images/workday-scheduler.png'} application={'https://kayla-sullens.github.io/Work-Day-Scheduler/'} repo={'https://github.com/Kayla-Sullens/Work-Day-Scheduler'} />
+          <Project name={'MVC Group Project'} description={'This was a group project done to show our knowledge of the MVC framework.'} imgAlt={'Image of a signin/signup page for Bookworm Connection.'} imgSrc={'./images/bookworm-connection.png'} application={''} repo={'https://github.com/Kayla-Sullens/Bookworm-Connection'}/>
+          <Project name={'Weather Dashboard'} description={'Check the weather for any city!'} imgAlt={'Image of search page for a weather dashboard.'} imgSrc={'./images/weather-dashboard.png'} application={'https://kayla-sullens.github.io/Weather-Dashboard/'} repo={'https://github.com/Kayla-Sullens/Weather-Dashboard'} />
+          <Project name={'Note Taker'} description={'Express project that allows users to take notes online, and save or delete them as needed.'} imgAlt={'Image of a note taker app that says Get Started.'} imgSrc={'./images/note-taker.png'} application={'https://agile-gorge-92951-b2ca71eaae04.herokuapp.com/'} repo={'https://github.com/Kayla-Sullens/Note-Taker'}/>
+          <Project name={'Coding Quiz'} description={'Quiz app to test knowledge of coding.'} imgAlt={'Image of coding quiz that shows a question and the timer.'} imgSrc={'./images/coding-quiz.png'} application={'https://kayla-sullens.github.io/Coding-Quiz-2/'} repo={'https://github.com/Kayla-Sullens/Coding-Quiz-2'}/>
+          <Project name={'Password Generator'} description={'Password generator to show JavaScript skills.'} imgAlt={'Image of password generator application.'} imgSrc={'./images/password-generator.png'} application={'https://kayla-sullens.github.io/Password-Generator-JS/'} repo={'https://github.com/Kayla-Sullens/Password-Generator-JS'}/>
+        
         </div>
       </section>
     </div>
